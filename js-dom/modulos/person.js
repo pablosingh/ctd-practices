dd3937f2d0b5f86d72cf27ee128891b576f9fc55
pablosingh <pablo22_4@hotmail.com>
2025-03-26 22:47:26 -0300

    Modulos

---
+++ js-dom/modulos/person.js
@@ -0,0 +1,8 @@
+export const name = 'Pedro';
+export const age = 30;
+
+// export { name, age } // para exportar todo junto
+
+const saludo = () => console.log('Hola');
+
+export default saludo;
---
+++ js-dom/modulos/script.js
@@ -0,0 +1,5 @@
+import { name, age } from './person.js';
+import puedoCambiarNombre from './person.js';
+
+console.log(name, age);
+puedoCambiarNombre();
