b02eb492c7836977508c63729a8b69f04c10fc48
pablosingh <pablo22_4@hotmail.com>
2025-03-29 18:05:38 -0300

    App Clima parte 03 peticion a api

--- js-dom/app-clima/script.js
+++ js-dom/app-clima/script.js
@@ -15,6 +15,40 @@ document.getElementById('searchButton').addEventListener('click', () => {
 function fetchWeather(city) {
   fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
     .then((data) => data.json())
-    .then((data) => console.log(data))
+    .then((data) => showWeather(data))
     .catch((error) => console.error(error));
+}
+
+function showWeather(data) {
+  console.log(data);
+  const divResponseData = document.getElementById('responseData');
+  divResponseData.innerHTML = '';
+
+  const city = data.name;
+  const countryName = data.sys.country;
+  const temp = data.main.temp;
+  const humidity = data.main.humidity;
+  const description = data.weather[0].description;
+  const icon = data.weather[0].icon;
+
+  const cityInfo = document.createElement('h2');
+  cityInfo.textContent = `${city}, ${countryName}`;
+
+  const tempInfo = document.createElement('p');
+  tempInfo.textContent = `La temperatura es : ${Math.floor(temp - diffKelvin)} °C`;
+
+  const humidityInfo = document.createElement('p');
+  humidityInfo.textContent = `La humedad es ${humidity} %`;
+
+  const iconInfo = document.createElement('img');
+  iconInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+  const descriptionInfo = document.createElement('p');
+  descriptionInfo.textContent = `Descripcion meteorologica: ${description}`;
+
+  divResponseData.appendChild(cityInfo);
+  divResponseData.appendChild(tempInfo);
+  divResponseData.appendChild(humidityInfo);
+  divResponseData.appendChild(iconInfo);
+  divResponseData.appendChild(descriptionInfo);
 }
