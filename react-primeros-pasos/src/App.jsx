8ac4276ad41337cfc9b7214f906e1db620f1ea3d
pablosingh <pablo22_4@hotmail.com>
2025-04-09 22:05:42 -0300

    PropTypes en React

--- react-primeros-pasos/src/App.jsx
+++ react-primeros-pasos/src/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function App() {
-  return <div>App</div>;
-}
+const App = ({ titulo, subtitulo }) => {
+  return (
+    <>
+      <h1>Props obligatorias y por Default</h1>
+      <h3>{titulo}</h3>
+      <h3>{subtitulo}</h3>
+    </>
+  );
+};
+
+App.propTypes = {
+  titulo: PropTypes.string.isRequired,
+  subtitulo: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  titulo: 'Este es un titulo por defecto',
+  subtitulo: 'Este es un subtitulo por defecto',
+};
+
+export default App;
--- react-primeros-pasos/src/main.jsx
+++ react-primeros-pasos/src/main.jsx
@@ -4,6 +4,6 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App titulo="Este es un titulo" />
   </StrictMode>
 );
