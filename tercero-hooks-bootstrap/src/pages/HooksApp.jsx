1bc064afb943c9b8061cff74552f6b5cdedc18e1
pablosingh <pablo22_4@hotmail.com>
2025-04-16 22:58:53 -0300

    Instalando bootstrap

---
+++ tercero-hooks-bootstrap/src/pages/HooksApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HooksApp() {
+  return (
+    <>
+      <h1>HooksApp</h1>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </>
+  );
+}
