57953e689f7e0dffad4605c9ecb82e2dd7d7617d
pablosingh <pablo22_4@hotmail.com>
2025-04-29 22:36:03 -0300

    Variables de entorno

--- react-clima/src/WeatherApp.jsx
+++ react-clima/src/WeatherApp.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WeatherApp.css';
 
 export const WeatherApp = () => {
-  const API_KEY = 'dc614accdf2a3284002e1619e8883d75';
-  const city = 'londres';
-  const urlBase = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=es`;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const urlBase = `https://api.openweathermap.org/data/2.5/weather`;
   const difKelvin = 273.15;
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
+    fetchWeatherData();
+  };
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`);
+      const data = await response.json();
+      setWeatherData(data);
+      //   console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <div className="container">
-      <h2>WeatherApp</h2>
+      <h1>WeatherApp</h1>
       <form onSubmit={handleSubmit}>
-        {/* <label htmlFor="city">Ciudad</label> */}
-        <input type="text" name="city" id="city" placeholder="Ingrese una ciudad" />
-        <button type="submit">Buscar</button>
+        <input type="text" value={city} name="city" id="city" placeholder="Ingrese una ciudad" onChange={handleCityChange} />
+        <button type="submit" onSubmit={handleSubmit}>
+          Buscar
+        </button>
       </form>
+      {weatherData && (
+        <div>
+          <h2>
+            {weatherData.name}, {weatherData.sys.country}
+          </h2>
+          <p>La temperatura actual es {Math.floor(weatherData.main.temp - difKelvin)}ºC</p>
+          <p>La condición meteorológica actual: {weatherData.weather[0].description}</p>
+          <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description} />
+        </div>
+      )}
     </div>
   );
 };
